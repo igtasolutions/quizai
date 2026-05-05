5a4311c0b66f6342331f3edbad8dc4a087ede9fe
Igor <igorffmelo2@gmail.com>
2026-05-05 13:37:51 -0300

    remove CSP header

--- next.config.ts
+++ next.config.ts
@@ -1,19 +1,5 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  async headers() {
-    return [
-      {
-        source: '/q/:slug*',
-        headers: [
-          {
-            key: 'Content-Security-Policy',
-            value: "frame-src 'self' https://www.youtube.com https://player.vimeo.com https://www.youtube-nocookie.com https://vturb.com.br https://cdn.pandavideo.com https://*.pandavideo.com;",
-          },
-        ],
-      },
-    ]
-  },
-}
+const nextConfig: NextConfig = {};
 
 export default nextConfig;
