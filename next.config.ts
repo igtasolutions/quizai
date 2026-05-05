1c60707e903b6e9f66798e1fd17d62e49e53e15c
Igor <igorffmelo2@gmail.com>
2026-05-05 09:27:02 -0300

    fix CSP youtube embed

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,19 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
-};
+  async headers() {
+    return [
+      {
+        source: '/q/:slug*',
+        headers: [
+          {
+            key: 'Content-Security-Policy',
+            value: "frame-src 'self' https://www.youtube.com https://player.vimeo.com https://www.youtube-nocookie.com https://vturb.com.br https://cdn.pandavideo.com https://*.pandavideo.com;",
+          },
+        ],
+      },
+    ]
+  },
+}
 
 export default nextConfig;
