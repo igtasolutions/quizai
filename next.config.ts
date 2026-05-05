a8a8d67387108ae11ce303b9fcde43a18c70160c
Igor <igorffmelo2@gmail.com>
2026-05-05 13:40:27 -0300

    fix CSP completo para YouTube e outros players

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,32 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {};
+const nextConfig: NextConfig = {
+  async headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: [
+          {
+            key: 'X-Frame-Options',
+            value: 'SAMEORIGIN',
+          },
+          {
+            key: 'Content-Security-Policy',
+            value: [
+              "default-src 'self'",
+              "script-src 'self' 'unsafe-inline' 'unsafe-eval' https://connect.facebook.net https://www.youtube.com https://www.youtube-nocookie.com",
+              "frame-src 'self' https://www.youtube.com https://www.youtube-nocookie.com https://player.vimeo.com https://vturb.com.br https://*.pandavideo.com https://*.kwik.video",
+              "img-src 'self' data: blob: https: http:",
+              "media-src 'self' https: blob:",
+              "connect-src 'self' https:",
+              "style-src 'self' 'unsafe-inline' https://fonts.googleapis.com",
+              "font-src 'self' https://fonts.gstatic.com",
+            ].join('; '),
+          },
+        ],
+      },
+    ]
+  },
+}
 
 export default nextConfig;
